b3eb5f933e0f48a0b4bb5bb901ed00c453344879
Fellipe Soares Oliveira <fellipe.soares@barad-dur.local>
2018-06-03 17:46:11 -0300

    Corrigindo GET request

--- src/main/java/br/com/app/horadorango/gateways/http/HoraDoRangoController.java
+++ src/main/java/br/com/app/horadorango/gateways/http/HoraDoRangoController.java
@@ -5,6 +5,7 @@ import br.com.app.horadorango.usecase.PlaceUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,8 +25,8 @@ public class HoraDoRangoController {
         return placeUseCase.listAllPlaces();
     }
 
-    @RequestMapping(value="/{placeId}", method = GET)
-    public Place getPlace(@PathVariable Integer placeId) {
+    @RequestMapping(method = GET)
+    public Place getPlace(@RequestParam("placeId") Integer placeId) {
         return placeUseCase.getMenuPlace(placeId);
     }
 
